86fb7826e329783b595a275196e7344958dea1ea
Eduardo Velasques <eduveks@gmail.com>
2025-01-16 22:16:55 +0000

    Types created.

---
+++ types.d.ts
@@ -0,0 +1,293 @@
+declare global {
+	export type byte = number;
+	export type short = number;
+	export type int = number;
+	export type long = number;
+	export type float = number;
+	export type double = number;
+	export type char = string;
+}
+export declare namespace com.google.firebase {
+	export type FirebaseApp = any;
+}
+export declare namespace com.google.firebase.auth {
+	export type FirebaseAuth = any;
+}
+export declare namespace com.google.firebase.database {
+	export type FirebaseDatabase = any;
+	export type DatabaseReference = any;
+}
+export declare namespace com.itextpdf.kernel.colors {
+	export type Color = any;
+	export type DeviceCmyk = any;
+	export type DeviceGray = any;
+	export type DeviceRgb = any;
+}
+export declare namespace com.itextpdf.kernel.font {
+	export type PdfFont = any;
+}
+export declare namespace com.itextpdf.kernel.geom {
+	export type PageSize = any;
+}
+export declare namespace com.itextpdf.kernel.pdf {
+	export type PdfDocument = any;
+	export type PdfPage = any;
+	export type PdfReader = any;
+	export type PdfWriter = any;
+}
+export declare namespace com.itextpdf.kernel.pdf.canvas {
+	export type PdfCanvas = any;
+}
+export declare namespace com.itextpdf.kernel.pdf.canvas.draw {
+	export type ILineDrawer = any;
+}
+export declare namespace com.itextpdf.layout {
+	export type Document = any;
+}
+export declare namespace com.itextpdf.layout.borders {
+	export type Border = any;
+	export type Border3D = any;
+}
+export declare namespace com.itextpdf.layout.element {
+	export type AreaBreak = any;
+	export type Cell = any;
+	export type Image = any;
+	export type Paragraph = any;
+	export type Table = any;
+	export type Text = any;
+}
+export declare namespace com.rabbitmq.client {
+	export type Channel = any;
+	export type Connection = any;
+	export type ConnectionFactory = any;
+}
+export declare namespace io.jsonwebtoken.security {
+	export type SignatureAlgorithm = any;
+	export type MacAlgorithm = any;
+}
+export declare namespace jakarta.websocket {
+	export type Session = any;
+}
+export declare namespace java.awt {
+	export type Color = any;
+	export type Image = any;
+	export type Font = any;
+	export type FontMetrics = any;
+	export type Graphics2D = any;
+	export type RenderingHints$Key = any;
+	export type RenderingHints = any;
+}
+export declare namespace java.awt.geom {
+	export type AffineTransform = any;
+	export type Rectangle2D = any;
+}
+export declare namespace java.awt.image {
+	export type BufferedImage = any;
+}
+export declare namespace java.io {
+	export type Reader = any;
+	export type Writer = any;
+	export type BufferedReader = any;
+	export type File = any;
+	export type PrintStream = any;
+	export type PrintWriter = any;
+}
+export declare namespace java.lang {
+	export type Object = any;
+	export type Class = any;
+	export type String = any;
+	export type Boolean = any;
+	export type Throwable = any;
+	export type StackTraceElement = any;
+	export type Runnable = any;
+	export type Error = any;
+	export type Exception = any;
+	export type CharSequence = any;
+	export type Iterable = any;
+}
+export declare namespace java.nio.charset {
+	export type Charset = any;
+}
+export declare namespace java.nio.file {
+	export type Path = any;
+}
+export declare namespace java.security {
+	export type SecureRandom = any;
+}
+export declare namespace java.sql {
+	export type Date = any;
+	export type Time = any;
+	export type Timestamp = any;
+}
+export declare namespace java.text {
+	export type AttributedString = any;
+	export type SimpleDateFormat = any;
+}
+export declare namespace java.time {
+	export type Instant = any;
+	export type LocalDate = any;
+	export type LocalDateTime = any;
+	export type LocalTime = any;
+	export type Clock = any;
+	export type ZoneId = any;
+	export type Duration = any;
+	export type Month = any;
+	export type ZoneOffset = any;
+	export type MonthDay = any;
+	export type OffsetDateTime = any;
+	export type OffsetTime = any;
+	export type Period = any;
+	export type Year = any;
+	export type YearMonth = any;
+	export type ZonedDateTime = any;
+}
+export declare namespace java.time.chrono {
+	export type ChronoLocalDate = any;
+}
+export declare namespace java.time.format {
+	export type DateTimeFormatter = any;
+	export type DateTimeFormatterBuilder = any;
+	export type DecimalStyle = any;
+}
+export declare namespace java.time.temporal {
+	export type Temporal = any;
+	export type TemporalAccessor = any;
+	export type ChronoField = any;
+	export type ChronoUnit = any;
+	export type TemporalAmount = any;
+	export type TemporalUnit = any;
+	export type IsoFields = any;
+	export type JulianFields = any;
+	export type ValueRange = any;
+}
+export declare namespace java.util {
+	export type Collection = any;
+	export type Calendar = any;
+	export type Date = any;
+	export type List = any;
+	export type Map = any;
+	export type UUID = any;
+	export type Set = any;
+	export type Iterator = any;
+	export type ListIterator = any;
+	export type Properties = any;
+	export type Comparator = any;
+	export type Spliterator = any;
+	export type Random = any;
+	export type Locale = any;
+	export type GregorianCalendar = any;
+	export type TimeZone = any;
+}
+export declare namespace java.util.concurrent {
+	export type TimeUnit = any;
+}
+export declare namespace java.util.function {
+	export type BiFunction = any;
+	export type Function = any;
+	export type Predicate = any;
+	export type BiConsumer = any;
+	export type Consumer = any;
+	export type UnaryOperator = any;
+}
+export declare namespace javax.crypto {
+	export type SecretKey = any;
+}
+export declare namespace javax.imageio.stream {
+	export type ImageInputStream = any;
+	export type ImageOutputStream = any;
+}
+export declare namespace javax.xml.parsers {
+	export type DocumentBuilder = any;
+	export type DocumentBuilderFactory = any;
+}
+export declare namespace org.apache.commons.csv {
+	export type CSVFormat = any;
+	export type CSVParser = any;
+	export type CSVPrinter = any;
+}
+export declare namespace org.apache.poi.hssf.usermodel {
+	export type HSSFSheet = any;
+}
+export declare namespace org.apache.poi.ss.usermodel {
+	export type Sheet = any;
+	export type ClientAnchor$AnchorType = any;
+	export type BorderStyle = any;
+	export type Cell = any;
+	export type CellStyle = any;
+	export type FillPatternType = any;
+	export type Font = any;
+	export type HorizontalAlignment = any;
+	export type ClientAnchor = any;
+	export type Picture = any;
+	export type Row = any;
+	export type VerticalAlignment = any;
+	export type Workbook = any;
+}
+export declare namespace org.apache.poi.ss.util {
+	export type CellRangeAddress = any;
+}
+export declare namespace org.graalvm.polyglot {
+	export type Value = any;
+}
+export declare namespace org.jsoup {
+	export type Connection = any;
+}
+export declare namespace org.jsoup.nodes {
+	export type Document$OutputSettings = any;
+	export type Document = any;
+}
+export declare namespace org.jsoup.safety {
+	export type Safelist = any;
+}
+export declare namespace org.netuno.proteu {
+	export type Proteu = any;
+	export type ProteuError = any;
+	export type Proteu$HTTPStatus = any;
+	export type Download = any;
+}
+export declare namespace org.netuno.psamata.ftp {
+	export type FTPClient = any;
+}
+export declare namespace org.netuno.psamata.io {
+	export type OutputStreamNotify = any;
+	export type IO = any;
+}
+export declare namespace org.netuno.psamata.mail {
+	export type IMAPClient = any;
+	export type Mail = any;
+}
+export declare namespace org.netuno.psamata.net {
+	export type Remote$Response = any;
+}
+export declare namespace org.netuno.psamata.ssh {
+	export type SSHClient = any;
+}
+export declare namespace org.netuno.tritao.com {
+	export type Configuration = any;
+}
+export declare namespace org.netuno.tritao.db {
+	export type DataItem$Status = any;
+	export type DataItem$StatusType = any;
+	export type Builder = any;
+}
+export declare namespace org.netuno.tritao.db.manager {
+	export type Column$Type = any;
+}
+export declare namespace org.netuno.tritao.query.join {
+	export type JoinType = any;
+	export type RelationType = any;
+}
+export declare namespace org.netuno.tritao.query.pagination {
+	export type Pagination = any;
+}
+export declare namespace org.netuno.tritao.query.where {
+	export type ConditionOperator = any;
+	export type RelationOperatorType = any;
+}
+export declare namespace org.netuno.tritao.resource {
+	export type Setup$RunResult = any;
+	export type WS$WSMessage = any;
+}
+export declare namespace org.w3c.dom {
+	export type Document = any;
+}
